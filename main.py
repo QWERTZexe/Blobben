63ca95e29d795725902641a4e469e4880c63663a
QWERTZ_EXE <qwertz@qwertz.app>
2024-06-24 13:36:06 +0200

    added home & reload icons, added better about blank page, fixed bugs

--- main.py
+++ main.py
@@ -4,11 +4,15 @@ from PyQt6.QtWidgets import QApplication, QMainWindow, QToolBar, QLineEdit, QPus
 from PyQt6.QtWebEngineWidgets import QWebEngineView
 from PyQt6.QtWebEngineCore import QWebEnginePage
 from PyQt6.QtGui import QIcon, QFont
+from PyQt6.QtWebEngineCore import QWebEngineUrlScheme, QWebEngineProfile
+from PyQt6.QtWebEngineCore import QWebEngineUrlSchemeHandler, QWebEngineUrlRequestJob
 
 # <a href="https://www.flaticon.com/free-icons/return" title="return icons">Return icons created by Kiranshastry - Flaticon</a>
+# <a href="https://www.flaticon.com/free-icons/reload" title="reload icons">Reload icons created by Uniconlabs - Flaticon</a>
+# <a href="https://www.flaticon.com/free-icons/home" title="home icons">Home icons created by Freepik - Flaticon</a>
 
 cwd = os.path.dirname(os.path.abspath(sys.argv[0]))
-
+    
 class CustomWebEnginePage(QWebEnginePage):
     def __init__(self, parent=None):
         super().__init__(parent)
@@ -21,7 +25,7 @@ class CustomWebEnginePage(QWebEnginePage):
                     window.location.href = 'https://www.bing.com/search?q=' + encodeURIComponent(searchTerm);
                 };
             """)
-    
+
 class Toolbar(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
@@ -47,10 +51,24 @@ class Toolbar(QWidget):
         self.forward_button.setIconSize(QSize(25, 25))
         self.toolbar.addWidget(self.forward_button)
 
+        # Reload button
+        self.reload_button = QPushButton()
+        self.reload_button.setIcon(QIcon(f"{cwd}/assets/reload"))
+        self.reload_button.setFixedSize(QSize(35, 35))
+        self.reload_button.setIconSize(QSize(25, 25))
+        self.toolbar.addWidget(self.reload_button)
+        
+        # Home button
+        self.home_button = QPushButton()
+        self.home_button.setIcon(QIcon(f"{cwd}/assets/home"))
+        self.home_button.setFixedSize(QSize(35, 35))
+        self.home_button.setIconSize(QSize(25, 25))
+        self.toolbar.addWidget(self.home_button)
+
         # URL bar
         self.url_bar = QLineEdit()
-        self.url_bar.setFixedHeight(35)
-        self.url_bar.setFont(QFont("Calibri", 15))
+        self.url_bar.setFixedHeight(30)
+        self.url_bar.setFont(QFont("Calibri", 13))
         self.toolbar.addWidget(self.url_bar)
 
         self.separator = QFrame()
@@ -67,17 +85,34 @@ class Toolbar(QWidget):
         # Connect signals
         self.back_button.clicked.connect(self.web_view.back)
         self.forward_button.clicked.connect(self.web_view.forward)
+        self.reload_button.clicked.connect(self.web_view.reload)
+        self.home_button.clicked.connect(lambda: self.web_view.setUrl(QUrl.fromLocalFile(f"{cwd}/sites/home.html")))
         self.url_bar.returnPressed.connect(self.navigate_to_url)
         self.web_view.urlChanged.connect(self.update_url)
 
     def navigate_to_url(self):
         q = QUrl(self.url_bar.text())
+        if self.url_bar.text() == "blobben://":
+            q = QUrl(f"file:///{cwd.replace("\\","/")}/sites/home.html")
+        elif self.url_bar.text().startswith("blobben://"):
+            q = QUrl(f"file:///{cwd.replace("\\","/")}/sites/{self.url_bar.text().replace("blobben://","")}" if self.url_bar.text().endswith(".html") else f"file:///{cwd.replace("\\","/")}/sites/{self.url_bar.text().replace("blobben://","")}.html")
         if q.scheme() == "":
-            q.setScheme("https://")
+            if not self.url_bar.text() == "":
+                q = QUrl(f"https://bing.com/search?q={self.url_bar.text()}")
+            else:
+                q = QUrl(f"file:///{cwd.replace("\\","/")}/sites/aboutblank.html")
         self.web_view.setUrl(q)
 
     def update_url(self, q):
-        self.url_bar.setText(q.toString())
+        a=f"file:///{cwd.replace("\\","/")}/sites/".lower()
+
+        if a.lower() in q.toString().lower():
+            self.url_bar.setText(f"{q.toString().lower().replace(a,"blobben://")}")
+            if self.url_bar.text().endswith(".html"):
+                self.url_bar.setText(self.url_bar.text().replace(".html",""))
+        else:
+            self.url_bar.setText(q.toString())
+
 
 class Browser(QMainWindow):
     def __init__(self):
